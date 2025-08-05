165ffc609fdea77359071c501afa85a79fedd251
fahd-111 <fahad.jcat123@gmail.com>
2025-08-06 02:33:19 +0500

    oAuth fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -50,16 +50,33 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async jwt({ token, user }) {
-      if (user) token.id = user.id;
-      return token;
-    },
-    async session({ session, token }) {
-      if (!session.user) {
-        session.user = { id: "", name: null, email: null, image: null };
+  async signIn({ user, account, profile }) {
+    if (account.provider === "google") {
+      const existingUser = await prisma.user.findUnique({
+        where: { email: user.email },
+      });
+      if (!existingUser) {
+        await prisma.user.create({
+          data: {
+            email: user.email,
+            name: user.name,
+            googleId: account.providerAccountId,
+          },
+        });
+        return true;
       }
-      if (token?.id) session.user.id = token.id as string;
-      return session;
-    },
+    }
+    return true;
   },
-};
+  async jwt({ token, user }) {
+    if (user) token.id = user.id;
+    return token;
+  },
+  async session({ session, token }) {
+    if (!session.user) {
+      session.user = { id: "", name: null, email: null, image: null };
+    }
+    if (token?.id) session.user.id = token.id as string;
+    return session;
+  },
+  };
