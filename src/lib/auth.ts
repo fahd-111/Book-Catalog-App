5f39e58b0d2b0290e12e0702686e1c037b8433ea
fahd-111 <fahad.jcat123@gmail.com>
2025-08-06 02:33:34 +0500

    oAuth fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -50,7 +50,7 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-  async signIn({ user, account, profile }) {
+  async signIn({ user, account }) {
     if (account.provider === "google") {
       const existingUser = await prisma.user.findUnique({
         where: { email: user.email },
